fc6b117f1fb4b2b7332ecf3a49346a2c8cacb55f
an5tka <anna.babaeva.2004@mail.ru>
2024-06-05 13:43:06 +0700

    Add files via upload

---
+++ 4 лаба.cs
@@ -0,0 +1,61 @@
+﻿using ClassLibrary;
+using System.Xml.Linq;
+
+public class SweetsGiftPrinter
+{
+    public void Print(double TotW)
+    {
+        Console.WriteLine("Вес подарка с конфетами: " + TotW);
+    }
+}
+public class SweetsGiftCalculator
+{
+    public double GetTotalWeight()
+    {
+        double totalWeight = 0;
+        int i;
+        for (i = 0; i < SweetsGift.Sweets.Count; i++)
+        {
+            totalWeight += SweetsGift.Sweets[i].Weight;
+        }
+        return totalWeight;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SweetsGift sweetsGift = SweetsGiftFactory.CreateSweetsGift();
+        SweetsGiftCalculator sweetsGiftCalculator = new SweetsGiftCalculator();
+        double totalWeight = sweetsGiftCalculator.GetTotalWeight();
+        SweetsGiftPrinter sweetsGiftPrinter = new SweetsGiftPrinter();
+        sweetsGiftPrinter.Print(totalWeight);
+
+    }
+}
+public class SweetsGiftFactory
+{
+    public static SweetsGift CreateSweetsGift()
+    {
+        CandiesSweets cn_sweets = new()
+        {
+            Name = "Баунти",
+            Weight = 260,
+            Type = "Сладкая",
+            Calories = 494
+        };
+        ChocolatesSweets ch_sweets = new()
+        {
+            Name = "Милка",
+            Weight = 190,
+            Taste = "Вкусная",
+            Brand = "Milka"
+        };
+        SweetsGift sweetsGift = new();
+        sweetsGift.AddSweetS(cn_sweets);
+        sweetsGift.AddSweetS(ch_sweets);
+
+        return sweetsGift;
+    }
+
+}
